46fba30529cf370c607ff083ec9bb9c4739a53c9
newfull5 <dhtocks@naver.com>
2022-01-12 23:46:27 +0900

    add Tester

---
+++ topic_classification/tester.py
@@ -0,0 +1,67 @@
+import torch
+from tqdm import tqdm
+
+
+class Tester:
+    def __init__(self, args, model, test_loader):
+        self.model = model.to(args.device).eval()
+        self.args = args
+        self.labels = []
+        self.pred = []
+        self.test_loader = test_loader
+        self.score = {}
+
+    def run(self):
+        for batch in tqdm(self.test_loader):
+            logits, _ = self.model(batch)
+            pred = torch.argmax(logits, dim=-1).tolist()
+
+            _, labels = batch
+            self.pred += pred
+            self.labels += labels.tolist()
+
+    def calc_score(self):
+        self._confusion_matrix()
+        self._f1_score()
+        self._accuracy()
+        print(self.score)
+
+    def _confusion_matrix(self):
+        for class_num in range(self.args.num_labels):
+            tp, tn, fp, fn = 0, 0, 0, 0
+            for i in range(len(self.pred)):
+                # Positive
+                if self.pred[i] == class_num:
+                    if self.pred[i] == self.labels[i]:
+                        tp += 1
+                    if self.pred[i] != self.labels[i]:
+                        fp += 1
+                # Negative
+                if self.pred[i] != class_num:
+                    if self.labels[i] != class_num:
+                        tn += 1
+                    if self.labels[i] == class_num:
+                        fn += 1
+
+            self.score[class_num] = {}
+            self.score[class_num]['confusion_matrix'] = (tp, tn, fp, fn)
+
+    def _f1_score(self):
+        for class_num in range(self.args.num_labels):
+            tp, tn, fp, fn = self.score[class_num]['confusion_matrix']
+
+            precision = tp / (tp + fp)
+            recall = tp / (tp + fn)
+            f1_score = 2 * precision * recall / (precision + recall)
+
+            self.score[class_num]['precision'] = precision
+            self.score[class_num]['recall'] = recall
+            self.score[class_num]['f1_score'] = f1_score
+
+    def _accuracy(self):
+        cnt = 0
+        for i in range(len(self.pred)):
+            if self.pred[i] == self.labels:
+                cnt += 1
+
+        self.score['accuracy'] = cnt / len(self.pred)
